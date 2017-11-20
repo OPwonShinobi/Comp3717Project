979b0a34f9b0fe239eeaa741e7225bb80dde51dc
Alex Xia <xiaalex1998@gmail.com>
2017-11-19 22:59:18 -0800

    press the map's 'You Are Here' FAB to goto your current location. If not found will go to heart of new westminster

--- app/src/main/java/com/example/comp3717project/comp3717project/GoogleMapsActivity.java
+++ app/src/main/java/com/example/comp3717project/comp3717project/GoogleMapsActivity.java
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -27,10 +28,13 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -50,7 +54,10 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
     private MapView mapView;
     private GoogleMap gMap;
     EditText et_address;
-//    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private Location mLastKnownLocation; //will zoom to this on FAB click
+    private final LatLng mDefaultLocation = new LatLng(49.205681, -122.911256); //google place new west name above this coord
+    private static final int DEFAULT_ZOOM = 15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,11 +83,14 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
 
         Button searchBtn = (Button) findViewById(R.id.map_btn);
         searchBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) { onSearch(); }
+            public void onClick(View view) {
+                onSearch();
+            }
         });
 
-        FloatingActionButton FAB = (FloatingActionButton)findViewById(R.id.locate_me_fab);
+        FloatingActionButton FAB = (FloatingActionButton) findViewById(R.id.locate_me_fab);
         FAB.setOnClickListener(new locateMeFABListener());
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this); //service used to locate phone location
     }
 
     public void onSearch() {
@@ -90,12 +100,11 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
         //moves keyboard away without clicking back
         View editText = this.getCurrentFocus();
         editText.clearAnimation();
-        InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+        InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
 
         //location will never be null unless et_address is not found roger!
-        if(!location.equals(""))
-        {
+        if (!location.equals("")) {
             Geocoder geocoder = new Geocoder(this);
             try {
                 //y is it a list, we only ever use 1
@@ -112,7 +121,7 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
             LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
             gMap.clear();
             gMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-            gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f));
+            gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
             //gMap.animateCamera(CameraUpdateFactory.zoomTo(5.0f));
         } else {
             Toast t = Toast.makeText(this, "Please enter an address.", Toast.LENGTH_LONG);
@@ -164,32 +173,61 @@ public class GoogleMapsActivity extends AppCompatActivity implements OnMapReadyC
 //            }
 //        }
         gMap = map;
-        if (map == null)
-        {
+        if (map == null) {
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
 
-            if(map != null)
-            {
-                map.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+            if (map != null) {
+                map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
                 //map.setMyLocationEnabled(true);
             }
         }
     }
 
+    private void gotoMyLocation() {
+        if (ActivityCompat.checkSelfPermission(GoogleMapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(GoogleMapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+        locationResult.addOnCompleteListener(GoogleMapsActivity.this, new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    // Set the map's camera position to the current location of the device.
+                    mLastKnownLocation = task.getResult();
+                    LatLng myLatLng = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                    gMap.clear();
+                    gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, DEFAULT_ZOOM));
+                    gMap.addMarker(new MarkerOptions().position(myLatLng).title("You are here"));
+                } else {
+                    Log.d("", "Current location is null. Using defaults.");
+                    Log.e("", "Exception: %s", task.getException());
+                    gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+                    //gMap.getUiSettings().setMyLocationButtonEnabled(false);
+                }
+            }
+        });
+    }
+
     private class locateMeFABListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            String msg = "Placeholder text.";
-            Snackbar allahu = Snackbar.make(findViewById(R.id.background_linear_layout), msg, Snackbar.LENGTH_LONG);
-            allahu.setAction("Undo", new View.OnClickListener(){
-                @Override
-                public void onClick(View view) {
-                    Toast burnt = Toast.makeText(GoogleMapsActivity.this, "Undid.", Toast.LENGTH_LONG);
-                    burnt.show();
-                }
-            });
-            allahu.show();
+            try {
+                gotoMyLocation();
+            } catch (SecurityException e)  {
+                Log.e("Exception: %s", e.getMessage());
+            }
         }
     }
 }
